d2666687858df9757156f71750bd54b5e6ccf105
hjpark <gowjd472@naver.com>
2015-03-04 15:42:20 +0900

    webapp package add

---
+++ src/main/java/webapp/model/Bouns.java
@@ -0,0 +1,14 @@
+package webapp.model;
+
+import java.util.List;
+
+public class Bouns {
+	String ename;
+	String job;
+	Float sal;
+	Float comm;
+	List<Emp>emps;
+	
+	
+
+}
---
+++ src/main/java/webapp/model/Salgrade.java
@@ -0,0 +1,12 @@
+package webapp.model;
+
+import java.util.List;
+
+public class Salgrade {
+	Float grade;
+	Float losal;
+	Float hisal;
+	
+	List<Emp>emps;
+
+}
